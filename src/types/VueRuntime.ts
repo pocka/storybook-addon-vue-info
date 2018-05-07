babb9ded0b9e8bf11c6ab36248d5cd811a3c5c0b
pocka <pocka.moe@gmail.com>
2018-05-07 19:03:03 +0900

    Refactor

--- src/types/VueRuntime.ts
+++ src/types/VueRuntime.ts
@@ -1,11 +1,21 @@
 import { ComponentOptions, PropOptions } from 'vue'
 
+import ComponentInfo from './ComponentInfo'
+
 export type RuntimeComponentOptions = ComponentOptions<any>
 
 export interface RuntimeComponent {
   name: string
   options: RuntimeComponentOptions
   extendedOptions: RuntimeComponentOptions
+}
+export const RuntimeComponent = {
+  toInfo(c: RuntimeComponent, name?: string): ComponentInfo {
+    return {
+      name: name || c.name || (c.options && c.options.name) || '',
+      component: c.options || c
+    }
+  }
 }
 
 export interface RuntimeComponents {
