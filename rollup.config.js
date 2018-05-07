0699536f0222e33f2a192beb80920ce084386cff
pocka <pocka.moe@gmail.com>
2018-05-07 12:52:44 +0900

    Compile js with rollup
    * Add prettier options
    * Use rollup-plugin-typescript2 (due to export error)

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import typescript from 'rollup-plugin-typescript2'
+import vue from 'rollup-plugin-vue'
+
+export default {
+  input: './src/index.ts',
+  output: {
+    file: './lib/index.js',
+    format: 'es'
+  },
+  plugins: [
+    typescript({
+      typescript: require('typescript')
+    }),
+    vue()
+  ]
+}
