df5e6ec83a48d195d83f1669f151efc6b9a00b43
pocka <pocka.moe@gmail.com>
2018-05-10 11:01:47 +0900

    Add markdown summary support

--- src/components/InfoDescription.vue
+++ src/components/InfoDescription.vue
@@ -1,11 +1,40 @@
+<script>
+export default {
+  props: {
+    descriptionHtml: {
+      type: String
+    }
+  },
+  created() {
+    console.log(this.descriptionHtml)
+  }
+}
+</script>
+
+
 <template>
-  <p class="summary">
-    <slot></slot>
-  </p>
+  <p class="summary" v-html="descriptionHtml"></p>
 </template>
 
 <style scoped>
 .summary {
   color: #777;
+}
+
+.summary>>>code,
+.summary>>>pre {
+  padding: 0.2em 0.5em;
+  background-color: #eee;
+  border-radius: 3px;
+}
+
+.summary>>>pre {
+  padding: 1em;
+}
+
+.summary>>>pre > code {
+  padding: 0;
+  background-color: transparent;
+  border-radius: 0;
 }
 </style>
