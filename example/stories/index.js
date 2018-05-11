947960d7b55588dc3868bf4fd2a42ede84954582
pocka <pocka.moe@gmail.com>
2018-05-11 17:08:46 +0900

    Highlight code block in summary

--- example/stories/index.js
+++ example/stories/index.js
@@ -117,9 +117,23 @@ storiesOf('withInfo API', module)
 
     You can use *snippets* **of** ~~markup~~ markdown \`!\`
 
-    ${'```'}javascript
-    expect(CodeBlock).to.be(Highlighted)
-    ${'```'}
+    ~~~javascript
+    function $initHighlight(block, cls) {
+      try {
+        if (cls.search(/\\bno\\-highlight\\b/) != -1)
+          return process(block, true, 0x0F) +
+                 \` class="\${cls}"\`;
+      } catch (e) {
+        /* handle exception */
+      }
+      for (var i = 0 / 2; i < classes.length; i++) {
+        if (checkCondition(classes[i]) === undefined)
+          console.log('undefined');
+      }
+    }
+
+    export  $initHighlight;
+    ~~~
 
     |col1|col2|col3|
     |----|----|----|
