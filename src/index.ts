d11151d69988f3a0f192e04e9714e154578094a6
pocka <pockawoooh@gmail.com>
2019-01-28 12:52:01 +0900

    [fix] Support for new decorator interface
    * @storybook/vue changed its .addDecorator API to wrap story component
      and made it difficult to access story component options. This commit
      add workaround for it and deprecation alert to the decorator.

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,14 @@ type StoryWrapper = (
   story: StoryFunction
 ) => (ctx: { kind: string; story: string }) => ComponentOptions<Vue>
 
-export function withInfo(options: Partial<InfoAddonOptions>): StoryWrapper
-export function withInfo(summary: string): StoryWrapper
+export function withInfo(
+  options: Partial<InfoAddonOptions>,
+  actualStoryComponent?: ComponentOptions<Vue>
+): StoryWrapper
+export function withInfo(
+  summary: string,
+  actualStoryComponent?: ComponentOptions<Vue>
+): StoryWrapper
 
 /**
  * Display story's information.
@@ -21,7 +27,8 @@ export function withInfo(summary: string): StoryWrapper
  *   .add('story', withInfo('summary')(() => '<foo/>'))
  */
 export function withInfo(
-  options: Partial<InfoAddonOptions> | string
+  options: Partial<InfoAddonOptions> | string,
+  actualStoryComponent?: ComponentOptions<Vue>
 ): StoryWrapper {
   return story => ctx => {
     // Normalize options and set defaults
@@ -31,7 +38,7 @@ export function withInfo(
     }
 
     // Get component options inside story
-    const storyResult = story()
+    const storyResult = actualStoryComponent || story()
     const storyComponent =
       typeof storyResult === 'string' ? { template: storyResult } : storyResult
 
@@ -63,7 +70,24 @@ export function setDefaults(options: Partial<InfoAddonOptions> | string): void {
  *   .addDecorator(VueInfoAddon)
  *   .add('story', () => '<foo/>')
  */
-export const VueInfoAddon: StoryDecorator = (story, ctx) =>
-  withInfo({})(story)(ctx)
+export const VueInfoAddon: StoryDecorator = (story, ctx) => {
+  console.warn(
+    "[storybook-addon-vue-info] DEPRECATE: Using this plugin with .addDecorator API is deprecated due to storybook's internal API changes."
+  )
+
+  // Since @storybook/vue@4.2.0-alpha.6, story component is wrapped
+  // by storybook and cannot get component options in straight.
+  let wrappedComponent
+
+  try {
+    const s = new (story() as any)()
+
+    if (s.$options && s.$options.STORYBOOK_WRAPS) {
+      wrappedComponent = s.$options.STORYBOOK_WRAPS.options
+    }
+  } catch (e) {}
+
+  return withInfo({}, wrappedComponent)(story)(ctx)
+}
 
 export default VueInfoAddon
