bcf2de5d60d078ea4d15fc1ab885a9cac30dac76
pocka <pockawoooh@gmail.com>
2019-01-31 13:10:23 +0900

    [chore] Add typing for .css files

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,11 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare module '*.css' {
+  const modules: {
+    [className: string]: string
+  }
+
+  export default modules
+}
