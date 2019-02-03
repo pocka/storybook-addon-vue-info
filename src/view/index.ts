40745122e8b80486c60970e9c38578fe6d8f20ec
pocka <pockawoooh@gmail.com>
2019-02-03 20:48:56 +0900

    [feat] Use decorator API
    * Switch to use decorator API. With this change and @storybook/4.2, we
      can now work with addon-knobs!

--- src/view/index.ts
+++ src/view/index.ts
@@ -1,30 +1,13 @@
-import Vue, { ComponentOptions } from 'vue'
-
 import addons from '@storybook/addons'
 
 import { Events } from '../addon'
 import { InfoAddonOptions } from '../options'
 import { StoryInfo } from '../types/info'
 
-export function wrap(
-  component: ComponentOptions<Vue>,
+export function renderToPanel(
   info: StoryInfo,
   options: InfoAddonOptions
-): ComponentOptions<Vue> {
-  return {
-    render(h) {
-      return h(
-        options.wrapperComponent,
-        {
-          props: { info, options }
-        },
-        [h(component)]
-      )
-    }
-  }
-}
-
-export function transfer(info: StoryInfo, options: InfoAddonOptions): void {
+): void {
   const channel = addons.getChannel()
 
   channel.emit(Events.ShowDocs, { info, options })
