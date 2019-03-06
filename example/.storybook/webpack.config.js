75c30982e879c51e7b51935683291b745e757428
pocka <pockawoooh@gmail.com>
2019-03-06 18:34:22 +0900

    [nits] Bump Storybook version (4->5)

--- example/.storybook/webpack.config.js
+++ example/.storybook/webpack.config.js
@@ -1,15 +1,15 @@
-module.exports = (baseConfig, env, defaultConfig) => {
-  defaultConfig.module.rules.push({
+module.exports = ({ config }) => {
+  config.module.rules.push({
     test: /\.vue$/,
     loader: 'storybook-addon-vue-info/loader',
     enforce: 'post'
   })
 
-  defaultConfig.module.rules.push({
+  config.module.rules.push({
     test: /\.stories\.js$/,
     loaders: [require.resolve('@storybook/addon-storysource/loader')],
     enforce: 'pre'
   })
 
-  return defaultConfig
+  return config
 }
