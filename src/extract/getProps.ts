e0c78de7ed9762abaaf5ec5f9b542b56f1458dae
Femi Oladeji <f.oladeji@temper.works>
2019-07-26 02:05:20 +0200

    get default value only if prop is specified as an object

--- src/extract/getProps.ts
+++ src/extract/getProps.ts
@@ -52,7 +52,7 @@ export function getProps(component: AnyComponent): PropInfo[] {
         default$ = 'Failed to get default value'
       }
     } else {
-      default$ = 'default' in propDef ? JSON.stringify(propDef.default) : ''
+      default$ = typeof propDef === 'object' && 'default' in propDef ? JSON.stringify(propDef.default) : ''
     }
 
     return {
